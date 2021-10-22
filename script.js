2a47bbc3c9bf6e4d571189366198cfdb7195b034
Paulo henrique <80927576+paulophlinf@users.noreply.github.com>
2021-10-22 12:21:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'paulophlinf',
+  youtube: 'maykbrito',
+  facebook: 'paulohenrique.lemos.71',
+  instagram: 'paulo_cotia',
+  twitter: 'maykbrito'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
